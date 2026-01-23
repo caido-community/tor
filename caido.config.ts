8c112b08f6de1adebea80fe35303b9857b39c9e6
Emile Fugulin <code@efugulin.com>
2026-01-22 22:26:12 -0500

    Bump version

--- caido.config.ts
+++ caido.config.ts
@@ -13,7 +13,7 @@ export default defineConfig({
   id,
   name: "Tor",
   description: "Allow connecting to the Tor network",
-  version: "0.0.0",
+  version: "0.1.0",
   author: {
     name: "Caido Labs Inc.",
     email: "dev@caido.io",
